4189a40d120d52a3f7af81a2e4e5ea42353f5b14
Raiver <raiver.figueroa@binaural.com.ve>
2021-04-23 11:00:11 -0400

    feat: campos legales en facturas y ordenes de compra y ventas hu:72

---
+++ modules/binaural_ventas/models/sale_inh.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+
+
+
+from werkzeug.urls import url_encode
+
+class SaleOrderBinauralVentas(models.Model):
+    _inherit = 'sale.order'
+
+    phone = fields.Char(string='Teléfono', related='partner_id.phone')
+    vat = fields.Char(string='RIF', compute='_get_vat')
+    address = fields.Char(string='Dirección', related='partner_id.street')
+    #business_name = fields.Char(string='Razón Social', related='partner_id.business_name')
+    
+    
+    @api.depends('partner_id')
+    def _get_vat(self):
+        for p in self:
+            #if self.partner_id.prefix_vat and self.partner_id.vat:
+            if p.partner_id.vat and p.partner_id.vat:
+                vat = str(p.partner_id.vat) #str(p.partner_id.prefix_vat) +
+            else:
+                vat = str(p.partner_id.vat)
+            p.vat = vat
