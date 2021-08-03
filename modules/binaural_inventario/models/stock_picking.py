1078fc2f728d6379d06341e59086b9c6a12837e6
Antony H <antony@binauraldev.com>
2021-08-03 10:52:59 -0400

    fix: error al transferir inventario

--- modules/binaural_inventario/models/stock_picking.py
+++ modules/binaural_inventario/models/stock_picking.py
@@ -16,17 +16,18 @@ class StockPickingBinauralInv(models.Model):
     @api.depends('origin')
     def _compute_foreign_currency(self):
         for record in self:
-            if len(record.origin) > 0:
+            foreign_currency_rate = foreign_currency_id = 0
+            if record.origin:
                 sale_id = record.env['sale.order'].search([('name', '=', record.origin)], limit=1)
                 if sale_id:
                     foreign_currency_id = sale_id.currency_id.id
                     foreign_currency_rate = sale_id.foreign_currency_rate
-                else:
-                    alternate_currency = int(record.env['ir.config_parameter'].sudo().get_param('curreny_foreign_id'))
-                    foreign_currency_id = alternate_currency
-                    rate = self.env['res.currency.rate'].search([('currency_id', '=', foreign_currency_id),
-                                                                 ('name', '<=', fields.Date.today())], limit=1,
-                                                                order='name desc')
-                    foreign_currency_rate = rate.rate
-                record.foreign_currency_id = foreign_currency_id
-                record.foreign_currency_rate = foreign_currency_rate
+            else:
+                alternate_currency = int(record.env['ir.config_parameter'].sudo().get_param('curreny_foreign_id'))
+                foreign_currency_id = alternate_currency
+                rate = self.env['res.currency.rate'].search([('currency_id', '=', foreign_currency_id),
+                                                             ('name', '<=', fields.Date.today())], limit=1,
+                                                            order='name desc')
+                foreign_currency_rate = rate.rate
+            record.foreign_currency_id = foreign_currency_id
+            record.foreign_currency_rate = foreign_currency_rate
