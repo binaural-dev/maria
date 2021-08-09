5531cef7e458028da067ff8f259d283cd10756f8
Antony H <antony@binauraldev.com>
2021-08-09 08:39:17 -0400

    fix: limpiar codigo no fiscal y tracking de campo #148

--- modules/binaural_facturacion/models/account_journal.py
+++ modules/binaural_facturacion/models/account_journal.py
@@ -6,4 +6,4 @@ from odoo import api, fields, models, _
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    fiscal = fields.Boolean(string="Fiscal")
+    fiscal = fields.Boolean(string="Fiscal", tracking=True)
