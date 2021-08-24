60689ed623eefc55e70401f0b6cc5dd68ca31cd5
Raiver <raiver.figueroa@binaural.com.ve>
2021-08-24 14:57:31 -0400

    feat:aumentar tamano de fuente,campos en frmulario,cantidad por almacen

--- custom/cumming_inventario/models/product_template.py
+++ custom/cumming_inventario/models/product_template.py
@@ -17,7 +17,16 @@ class ProductTemplateCummingInventario(models.Model):
 	_inherit = 'product.template'
 
 	pattern_id = fields.Many2one('product.pattern',string='Modelo')
-
+	fob_cost = fields.Float(string='Costo FOB')
+	percent_dif_cost = fields.Float(string='% DIF CIF y FOB')
+	standard_price = fields.Float(
+		'Costo CIF', compute='_compute_standard_price',
+		inverse='_set_standard_price', search='_search_standard_price',
+		digits='Product Price', groups="base.group_user",
+		help="""In Standard Price & AVCO: value of the product (automatically computed in AVCO).
+		In FIFO: value of the last unit that left the stock (automatically computed).
+		Used to value the product when the purchase cost is not known (e.g. inventory adjustment).
+		Used to compute margins on sale orders.""")
 
 class PatternReportStock(models.Model):
 	_inherit = 'stock.quant'
