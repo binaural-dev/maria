33147b3e6ebd41c29234c8469241092f1f8163c3
Antony Herrera <antony@binauraldev.com>
2021-09-13 10:26:14 -0400

    fix: change widget

--- modules/binaural_reporte_fiscal/static/src/js/account_reports.js
+++ modules/binaural_reporte_fiscal/static/src/js/account_reports.js
@@ -2,17 +2,18 @@ odoo.define('binaural_reporte_fiscal.accountReportsWidget_inherit', function (re
 'use strict';
 
 var core = require('web.core');
-var accountReportsWidget = require('account_reports.account_report').GeneratePriceList;
+var accountReportsWidget = require('account_reports.account_report').accountReportsWidget;
+var QWeb = core.qweb;
+var _t = core._t;
 
-var accountReportsWidgetInherit = accountReportsWidget.include({
-    hasControlPanel: true,
+var accountReportsWidgetInherit = accountReportsWidget.extend({
         custom_events: _.extend({}, accountReportsWidget.prototype.custom_events, {
              'click .o_action': '_onClickAction',
         }),
 });
 
 console.log('testinggggggggggggggg')
-core.action_registry.add('account_report', accountReportsWidget);
+core.action_registry.add('account_report', accountReportsWidgetInherit);
 
 return accountReportsWidgetInherit;
 
