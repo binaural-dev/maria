6908418b416151532af1630afba1de19c7b4c092
Antony Herrera <antony@binauraldev.com>
2021-09-22 14:40:55 -0400

    fix: Editar Nombre de menú Balance de situación a Estado de situación financiera #206

--- modules/binaural_reporte_fiscal/report/account_general_ledger.py
+++ modules/binaural_reporte_fiscal/report/account_general_ledger.py
@@ -1,8 +1,5 @@
 
 from odoo import models, fields, api, _
-from odoo.tools.misc import format_date, DEFAULT_SERVER_DATE_FORMAT
-from datetime import timedelta
-from pprint import pprint
 
 
 class AccountGeneralLedgerReportBinaural(models.AbstractModel):
