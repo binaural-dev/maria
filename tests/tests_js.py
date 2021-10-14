a46c3c92a8c04a1994ad7074c72913b8de3134a2
hachhe <mayinis@binauraldev.com>
2021-10-14 13:39:02 -0400

    Añadir modulo

--- tests/tests_js.py
+++ tests/tests_js.py
@@ -1,18 +1,14 @@
-# coding: utf-8
-import odoo.tests
+# EJEMPLO DE PRUEBAS EN PHANTOMJS ----- LANZADOR CON PYTHON
+# EN ODOO +14 SE UTILIZA START_TOUR O BROWSER_TOUR 
 
-@odoo.tests.common.at_install(False)
-@odoo.tests.common.post_install(True)
+from odoo.addons.base.tests.common import HttpCase
+from odoo.tests import tagged
+
+@tagged('post_install', '-at_install')
 class TestUi(odoo.tests.HttpCase):
     def test_01_accouting_retenciones(self):
-        # Este fragmento aun esta en prueba (variable code luego de link)
-        # code = """
-        #     setTimeout(function () {
-        #         $(".account_accountant").click();
-        #         setTimeout(function () {console.log('ok');}, 3000);
-        #     }, 1000);
-        # """
         link = '/web'
-        self.phantom_js(link, "odoo.__DEBUG__.services['web_tour.tour'].run('account_retenciones')",
-            "odoo.__DEBUG__.services['web_tour.tour'].tours.account_retenciones.ready",  login="admin")
+        self.start_tour(link, 'test_ventas', login = "admin")
+
+
 
