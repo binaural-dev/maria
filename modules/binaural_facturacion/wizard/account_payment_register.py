21558b274c4afff0017664212480db36e7e2d6c2
Raiver <raiver.figueroa@binaural.com.ve>
2021-10-21 15:27:07 -0400

    fix:tasa

--- modules/binaural_facturacion/wizard/account_payment_register.py
+++ modules/binaural_facturacion/wizard/account_payment_register.py
@@ -23,25 +23,142 @@ class AccountPaymentRegisterBinauralFacturacion(models.TransientModel):
     @api.onchange('foreign_currency_id', 'foreign_currency_date')
     def _compute_foreign_currency_rate(self):
         for record in self:
-            rate = self._get_rate(record.foreign_currency_id.id, record.foreign_currency_date, '<=')
+            if record.foreign_currency_rate == 0:
+                rate = self._get_rate(record.foreign_currency_id.id, record.foreign_currency_date, '<=')
 
-            if rate:
-                record.update({
-                    'foreign_currency_rate': rate.rate,
-                })
-            else:
-                rate = self._get_rate(record.foreign_currency_id.id, record.foreign_currency_date, '>=')
                 if rate:
                     record.update({
                         'foreign_currency_rate': rate.rate,
                     })
                 else:
-                    record.update({
-                        'foreign_currency_rate': 0.00,
-                    })
+                    rate = self._get_rate(record.foreign_currency_id.id, record.foreign_currency_date, '>=')
+                    if rate:
+                        record.update({
+                            'foreign_currency_rate': rate.rate,
+                        })
+                    else:
+                        record.update({
+                            'foreign_currency_rate': 0.00,
+                        })
 
     def _get_rate(self, foreign_currency_id, foreign_currency_date, operator):
         rate = self.env['res.currency.rate'].search([('currency_id', '=', foreign_currency_id),
                                                      ('name', operator, foreign_currency_date)], limit=1,
                                                     order='name desc')
         return rate
+
+
+    def _create_payment_vals_from_wizard(self):
+        payment_vals = {
+            'date': self.payment_date,
+            'amount': self.amount,
+            'payment_type': self.payment_type,
+            'partner_type': self.partner_type,
+            'ref': self.communication,
+            'journal_id': self.journal_id.id,
+            'currency_id': self.currency_id.id,
+            'partner_id': self.partner_id.id,
+            'partner_bank_id': self.partner_bank_id.id,
+            'payment_method_id': self.payment_method_id.id,
+            'destination_account_id': self.line_ids[0].account_id.id,
+            'foreign_currency_rate':self.foreign_currency_rate,
+        }
+
+        if not self.currency_id.is_zero(self.payment_difference) and self.payment_difference_handling == 'reconcile':
+            payment_vals['write_off_line_vals'] = {
+                'name': self.writeoff_label,
+                'amount': self.payment_difference,
+                'account_id': self.writeoff_account_id.id,
+            }
+        return payment_vals
+
+
+    """def _create_payment_vals_from_batch(self, batch_result):
+        batch_values = self._get_wizard_values_from_batch(batch_result)
+        _logger.info("batch_values %s",batch_values)
+        return {
+            'date': self.payment_date,
+            'amount': batch_values['source_amount_currency'],
+            'payment_type': batch_values['payment_type'],
+            'partner_type': batch_values['partner_type'],
+            'ref': self._get_batch_communication(batch_result),
+            'journal_id': self.journal_id.id,
+            'currency_id': batch_values['source_currency_id'],
+            'partner_id': batch_values['partner_id'],
+            'partner_bank_id': batch_result['key_values']['partner_bank_id'],
+            'payment_method_id': self.payment_method_id.id,
+            'destination_account_id': batch_result['lines'][0].account_id.id
+        }
+
+    @api.model
+    def _get_wizard_values_from_batch(self, batch_result):
+        ''' Extract values from the batch passed as parameter (see '_get_batches')
+        to be mounted in the wizard view.
+        :param batch_result:    A batch returned by '_get_batches'.
+        :return:                A dictionary containing valid fields
+        '''
+        _logger.info("batch_result %s",batch_result)
+        key_values = batch_result['key_values']
+        _logger.info("key_values %s",key_values)
+        lines = batch_result['lines']
+        company = lines[0].company_id
+
+        source_amount = abs(sum(lines.mapped('amount_residual')))
+        if key_values['currency_id'] == company.currency_id.id:
+            source_amount_currency = source_amount
+        else:
+            source_amount_currency = abs(sum(lines.mapped('amount_residual_currency')))
+
+        return {
+            'company_id': company.id,
+            'partner_id': key_values['partner_id'],
+            'partner_type': key_values['partner_type'],
+            'payment_type': key_values['payment_type'],
+            'source_currency_id': key_values['currency_id'],
+            'source_amount': source_amount,
+            'source_amount_currency': source_amount_currency,
+        }
+
+    def _get_batches(self):
+        ''' Group the account.move.line linked to the wizard together.
+        :return: A list of batches, each one containing:
+            * key_values:   The key as a dictionary used to group the journal items together.
+            * moves:        An account.move recordset.
+        '''
+        self.ensure_one()
+
+        lines = self.line_ids._origin
+        _logger.info("lines %s",lines)
+        if len(lines.company_id) > 1:
+            raise UserError(_("You can't create payments for entries belonging to different companies."))
+        if not lines:
+            raise UserError(_("You can't open the register payment wizard without at least one receivable/payable line."))
+
+        batches = {}
+        for line in lines:
+            batch_key = self._get_line_batch_key(line)
+
+            serialized_key = '-'.join(str(v) for v in batch_key.values())
+            batches.setdefault(serialized_key, {
+                'key_values': batch_key,
+                'lines': self.env['account.move.line'],
+            })
+            batches[serialized_key]['lines'] += line
+        return list(batches.values())"""
+
+
+    @api.model
+    def _get_line_batch_key(self, line):
+        ''' Turn the line passed as parameter to a dictionary defining on which way the lines
+        will be grouped together.
+        :return: A python dictionary.
+        '''
+        return {
+            'partner_id': line.partner_id.id,
+            'account_id': line.account_id.id,
+            'foreign_currency_rate':line.foreign_currency_rate,
+            'currency_id': (line.currency_id or line.company_currency_id).id,
+            'partner_bank_id': line.move_id.partner_bank_id.id,
+            'partner_type': 'customer' if line.account_internal_type == 'receivable' else 'supplier',
+            'payment_type': 'inbound' if line.balance > 0.0 else 'outbound',
+        }
