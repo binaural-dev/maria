d92089237f55b7f5feb94046f95b03509987fa98
Raiver <raiver.figueroa@binaural.com.ve>
2021-10-21 14:38:02 -0400

    fix:tasa en wizard

--- modules/binaural_facturacion/wizard/account_payment_register.py
+++ modules/binaural_facturacion/wizard/account_payment_register.py
@@ -45,3 +45,28 @@ class AccountPaymentRegisterBinauralFacturacion(models.TransientModel):
                                                      ('name', operator, foreign_currency_date)], limit=1,
                                                     order='name desc')
         return rate
+
+
+    def _create_payment_vals_from_wizard(self):
+        payment_vals = {
+            'date': self.payment_date,
+            'amount': self.amount,
+            'payment_type': self.payment_type,
+            'partner_type': self.partner_type,
+            'ref': self.communication,
+            'journal_id': self.journal_id.id,
+            'currency_id': self.currency_id.id,
+            'partner_id': self.partner_id.id,
+            'partner_bank_id': self.partner_bank_id.id,
+            'payment_method_id': self.payment_method_id.id,
+            'destination_account_id': self.line_ids[0].account_id.id,
+            'foreign_currency_rate':self.foreign_currency_rate,
+        }
+
+        if not self.currency_id.is_zero(self.payment_difference) and self.payment_difference_handling == 'reconcile':
+            payment_vals['write_off_line_vals'] = {
+                'name': self.writeoff_label,
+                'amount': self.payment_difference,
+                'account_id': self.writeoff_account_id.id,
+            }
+        return payment_vals
