f036ac7f38c7e113f55096c8c8d73508065c25e8
Raiver <raiver.figueroa@binaural.com.ve>
2021-11-22 10:33:12 -0400

    fix:removiendo carpeta custom

--- custom/Country_Gastos/models/account_payment_inh.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api, exceptions, _
-
-
-class account_payment_inh_expense(models.Model):
-	_inherit = 'account.payment'
-
-	is_expense = fields.Boolean(default=False, string="Es Gasto", help="Este pago es un gasto")
--- custom/country_eventos/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_config_event
-from . import registration_editor
