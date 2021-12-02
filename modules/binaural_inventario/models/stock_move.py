9b7fddd73d9fcb87346947ca057f2520409a849e
Raiver <raiver.figueroa@binaural.com.ve>
2021-12-02 10:46:40 -0400

    feat:usar tasa de sistema en ajustes de inventario

--- modules/binaural_inventario/models/stock_move.py
+++ modules/binaural_inventario/models/stock_move.py
@@ -9,12 +9,22 @@ _logger = logging.getLogger(__name__)
 class StockMoveBinauralInventario(models.Model):
 	_inherit = "stock.move"
 
+	def _get_def_rate(self):
+		alternate_currency = int(self.env['ir.config_parameter'].sudo().get_param('curreny_foreign_id'))
+		foreign_currency_id = alternate_currency
+		rate = self.env['res.currency.rate'].search([('currency_id', '=', foreign_currency_id),
+				('name', '<=', fields.Date.today())], limit=1,
+				order='name desc')
+		def_rate = rate.rate if rate else 0
+		return def_rate
+
 	def _create_account_move_line(self, credit_account_id, debit_account_id, journal_id, qty, description, svl_id,
 								  cost):
 		self.ensure_one()
 		AccountMove = self.env['account.move'].with_context(default_journal_id=journal_id)
 
 		move_lines = self._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id, description)
+		def_rate = self._get_def_rate()
 		if move_lines:
 			date = self._context.get('force_period_date', fields.Date.context_today(self))
 			new_account_move = AccountMove.sudo().create({
@@ -25,7 +35,7 @@ class StockMoveBinauralInventario(models.Model):
 				'stock_move_id': self.id,
 				'stock_valuation_layer_ids': [(6, None, [svl_id])],
 				'move_type': 'entry',
-				'foreign_currency_rate': self.picking_id.foreign_currency_rate,
+				'foreign_currency_rate': self.picking_id.foreign_currency_rate if self.picking_id.foreign_currency_rate else def_rate,
 			})
 			new_account_move._post()
 
