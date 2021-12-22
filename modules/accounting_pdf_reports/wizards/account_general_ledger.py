8bc68b4c83c44d3f826f3c6ed477e09c349fdc89
Raiver <raiver.figueroa@binaural.com.ve>
2021-12-22 17:49:05 -0400

    feat:libro mayor en bs

--- modules/accounting_pdf_reports/wizards/account_general_ledger.py
+++ modules/accounting_pdf_reports/wizards/account_general_ledger.py
@@ -3,7 +3,7 @@
 from odoo import fields, models, _
 from odoo.exceptions import UserError
 
-
+#libro mayor
 class AccountReportGeneralLedger(models.TransientModel):
     _inherit = "account.common.account.report"
     _name = "account.report.general.ledger"
@@ -13,10 +13,12 @@ class AccountReportGeneralLedger(models.TransientModel):
                                     help='If you selected date, this field allow you to add a row to display the amount of debit/credit/balance that precedes the filter you\'ve set.')
     sortby = fields.Selection([('sort_date', 'Date'), ('sort_journal_partner', 'Journal & Partner')], string='Sort by', required=True, default='sort_date')
     journal_ids = fields.Many2many('account.journal', 'account_report_general_ledger_journal_rel', 'account_id', 'journal_id', string='Journals', required=True)
+    another_currency = fields.Boolean(string='En Bolivares')
 
     def _print_report(self, data):
         data = self.pre_print_report(data)
         data['form'].update(self.read(['initial_balance', 'sortby'])[0])
+        data['form'].update(self.read(['another_currency'])[0])
         if data['form'].get('initial_balance') and not data['form'].get('date_from'):
             raise UserError(_("You must define a Start Date"))
         records = self.env[data['model']].browse(data.get('ids', []))
