430bf421f222043884b0463484ad2a5180a55984
Miguel Gozaine <miguel@binauraldev.com>
2022-02-22 16:58:39 -0400

    fix: Avance Cambio de campos de moneda alterna en vista de formulario de ISLR.

--- modules/binaural_facturacion/models/account_move_fields.py
+++ modules/binaural_facturacion/models/account_move_fields.py
@@ -78,3 +78,37 @@ class AccountMove(models.Model):
                 foreign_retention_amount.set("string", "Monto Retenido Bs.F")
                 res["fields"]["retention_islr_line_ids"]["views"]["tree"]["arch"] = etree.tostring(doc, encoding="unicode")
         return res
+
+
+class AccountRetention(models.Model):
+    _iherit = "account.retention"
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type=False, toolbar=False, submenu=False):
+        res = super().fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
+        foreign_currency_id = self.env["ir.config_parameter"].sudo().get_param("curreny_foreign_id")
+        if foreign_currency_id and foreign_currency_id == '2':
+            if view_type == "form":
+                doc = etree.XML(res["fields"]["retention_lie"]["views"]["tree"]["arch"])
+                foreign_facture_amount = doc.xpath("//field[@name='foreign_facture_amount']")[0]
+                foreign_facture_amount.set("string", "Base Imponible $")
+                foreign_facture_total = doc.xpath("//field[@name='foreign_facture_total']")[0]
+                foreign_facture_total.set("string", "Total Factura $")
+                foreign_iva_amount = doc.xpath("//field[@name='foreign_iva_amount']")[0]
+                foreign_iva_amount.set("string", "Iva Factura $")
+                foreign_retention_amount = doc.xpath("//field[@name='foreign_retention_amount']")[0]
+                foreign_retention_amount.set("string", "Monto Retenido $")
+                res["fields"]["retention_line"]["views"]["tree"]["arch"] = etree.tostring(doc, encoding="unicode")
+        elif foreign_currency_id and foreign_currency_id == '3':
+            if view_type == "form":
+                doc = etree.XML(res["fields"]["retention_lie"]["views"]["tree"]["arch"])
+                foreign_facture_amount = doc.xpath("//field[@name='foreign_facture_amount']")[0]
+                foreign_facture_amount.set("string", "Base Imponible Bs.F")
+                foreign_facture_total = doc.xpath("//field[@name='foreign_facture_total']")[0]
+                foreign_facture_total.set("string", "Total Factura Bs.F")
+                foreign_iva_amount = doc.xpath("//field[@name='foreign_iva_amount']")[0]
+                foreign_iva_amount.set("string", "Iva Factura Bs.F")
+                foreign_retention_amount = doc.xpath("//field[@name='foreign_retention_amount']")[0]
+                foreign_retention_amount.set("string", "Monto Retenido Bs.F")
+                res["fields"]["retention_line"]["views"]["tree"]["arch"] = etree.tostring(doc, encoding="unicode")
+        return res
