5a94aa9d307b0fb15b50c1467775b0c0ea162541
Miguel Gozaine <miguel@binauraldev.com>
2022-02-22 14:43:26 -0400

    fix: Editar etiquetas de moneda alterna en facturas y NC de proveedores y clientes #385

--- modules/binaural_facturacion/models/account_move_fields.py
+++ modules/binaural_facturacion/models/account_move_fields.py
@@ -10,17 +10,31 @@ class AccountMove(models.Model):
         res = super().fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
         foreign_currency_id = self.env["ir.config_parameter"].sudo().get_param("curreny_foreign_id")
         if foreign_currency_id and foreign_currency_id == '2':
-            doc = etree.XML(res["arch"])
             if view_type == "tree":
+                doc = etree.XML(res["arch"])
                 foreign_amount_total = doc.xpath("//field[@name='foreign_amount_total']")
                 if foreign_amount_total:
                     foreign_amount_total[0].set("string", "Total Moneda $")
                     res["arch"] = etree.tostring(doc, encoding="unicode")
+            elif view_type == "form":
+                doc = etree.XML(res["fields"]["invoice_line_ids"]["views"]["tree"]["arch"])
+                foreign_price_unit = doc.xpath("//field[@name='foreign_price_unit']")[0]
+                foreign_price_unit.set("string", "Precio $")
+                foreign_subtotal = doc.xpath("//field[@name='foreign_subtotal']")[0]
+                foreign_subtotal.set("string", "Subtotal $")
+                res["fields"]["invoice_line_ids"]["views"]["tree"]["arch"] = etree.tostring(doc, encoding="unicode")
         elif foreign_currency_id and foreign_currency_id == '3':
-            doc = etree.XML(res["arch"])
             if view_type == "tree":
+                doc = etree.XML(res["arch"])
                 foreign_amount_total = doc.xpath("//field[@name='foreign_amount_total']")
                 if foreign_amount_total:
                     foreign_amount_total[0].set("string", "Total Moneda Bs.F")
                     res["arch"] = etree.tostring(doc, encoding="unicode")
+            elif view_type == "form":
+                doc = etree.XML(res["fields"]["invoice_line_ids"]["views"]["tree"]["arch"])
+                foreign_price_unit = doc.xpath("//field[@name='foreign_price_unit']")[0]
+                foreign_price_unit.set("string", "Precio Bs.F")
+                foreign_subtotal = doc.xpath("//field[@name='foreign_subtotal']")[0]
+                foreign_subtotal.set("string", "Subtotal Bs.F")
+                res["fields"]["invoice_line_ids"]["views"]["tree"]["arch"] = etree.tostring(doc, encoding="unicode")
         return res
