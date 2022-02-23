fc7fc4cc79698fc62a5286f99639f596b4b2f7e2
Luis <luis@binauraldev.com>
2022-02-23 12:25:35 -0400

    fix: conflictos en merge request

--- modules/binaural_compras/models/__init__.py
+++ modules/binaural_compras/models/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-<<<<<<< modules/binaural_compras/models/__init__.py
 from . import purchase_fields
 from . import purchase_inh
 from . import inherited_purchase_line
-=======
-from . import purchase_fields
-from . import purchase_inh
->>>>>>> modules/binaural_compras/models/__init__.py
--- modules/binaural_ventas/models/__init__.py
+++ modules/binaural_ventas/models/__init__.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import res_config
-<<<<<<< modules/binaural_ventas/models/__init__.py
 from . import sale_fields
 from . import sale_inh
 from . import inherited_sale_line
 from . import inherited_sale_advanced
-=======
-from . import sale_fields
-from . import sale_inh
->>>>>>> modules/binaural_ventas/models/__init__.py
