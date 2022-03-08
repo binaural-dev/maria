b2eadf2d6b7466aea3ea396b686bb38fcdf99d4e
Miguel Gozaine <miguel@binauraldev.com>
2022-03-08 09:08:21 -0400

    #396 #397 fix: Ajuste vista de retenciones de ISLR e IVA de clientes.

--- modules/binaural_facturacion/models/account_fields.py
+++ modules/binaural_facturacion/models/account_fields.py
@@ -106,29 +106,26 @@ class AccountRetention(models.Model):
     @api.model
     def fields_view_get(self, view_id=None, view_type=False, toolbar=False, submenu=False):
         res = super().fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
-        foreign_currency_id = self.env["ir.config_parameter"].sudo().get_param("curreny_foreign_id")
-        if foreign_currency_id and foreign_currency_id == '2':
-            if view_type == "form":
-                doc = etree.XML(res["fields"]["retention_line"]["views"]["tree"]["arch"])
-                foreign_facture_amount = doc.xpath("//field[@name='foreign_facture_amount']")[0]
-                foreign_facture_amount.set("string", "Base Imponible $")
-                foreign_facture_total = doc.xpath("//field[@name='foreign_facture_total']")[0]
-                foreign_facture_total.set("string", "Total Factura $")
-                foreign_iva_amount = doc.xpath("//field[@name='foreign_iva_amount']")[0]
-                foreign_iva_amount.set("string", "Iva Factura $")
-                foreign_retention_amount = doc.xpath("//field[@name='foreign_retention_amount']")[0]
-                foreign_retention_amount.set("string", "Monto Retenido $")
-                res["fields"]["retention_line"]["views"]["tree"]["arch"] = etree.tostring(doc, encoding="unicode")
-        elif foreign_currency_id and foreign_currency_id == '3':
-            if view_type == "form":
-                doc = etree.XML(res["fields"]["retention_line"]["views"]["tree"]["arch"])
-                foreign_facture_amount = doc.xpath("//field[@name='foreign_facture_amount']")[0]
-                foreign_facture_amount.set("string", "Base Imponible Bs.F")
-                foreign_facture_total = doc.xpath("//field[@name='foreign_facture_total']")[0]
-                foreign_facture_total.set("string", "Total Factura Bs.F")
-                foreign_iva_amount = doc.xpath("//field[@name='foreign_iva_amount']")[0]
-                foreign_iva_amount.set("string", "Iva Factura Bs.F")
-                foreign_retention_amount = doc.xpath("//field[@name='foreign_retention_amount']")[0]
-                foreign_retention_amount.set("string", "Monto Retenido Bs.F")
-                res["fields"]["retention_line"]["views"]["tree"]["arch"] = etree.tostring(doc, encoding="unicode")
+        # foreign_currency_id = self.env["ir.config_parameter"].sudo().get_param("curreny_foreign_id")
+        if view_type == "form":
+            doc = etree.XML(res["fields"]["retention_line"]["views"]["tree"]["arch"])
+            facture_amount = doc.xpath("//field[@name='facture_amount']")[0]
+            facture_amount.set("string", "Base Imponible Bs.F")
+            facture_total = doc.xpath("//field[@name='facture_total']")[0]
+            facture_total.set("string", "Total Factura Bs.F")
+            iva_amount = doc.xpath("//field[@name='iva_amount']")[0]
+            iva_amount.set("string", "Iva Factura Bs.F")
+            retention_amount = doc.xpath("//field[@name='retention_amount']")[0]
+            retention_amount.set("string", "Monto Retenido Bs.F")
+
+
+            foreign_facture_amount = doc.xpath("//field[@name='foreign_facture_amount']")[0]
+            foreign_facture_amount.set("string", "Base Imponible Bs.F")
+            foreign_facture_total = doc.xpath("//field[@name='foreign_facture_total']")[0]
+            foreign_facture_total.set("string", "Total Factura Bs.F")
+            foreign_iva_amount = doc.xpath("//field[@name='foreign_iva_amount']")[0]
+            foreign_iva_amount.set("string", "Iva Factura Bs.F")
+            foreign_retention_amount = doc.xpath("//field[@name='foreign_retention_amount']")[0]
+            foreign_retention_amount.set("string", "Monto Retenido Bs.F")
+            res["fields"]["retention_line"]["views"]["tree"]["arch"] = etree.tostring(doc, encoding="unicode")
         return res
